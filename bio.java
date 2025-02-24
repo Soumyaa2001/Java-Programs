95a0a31be1e929f1588917866c14d60c0785147e
Soumyaneel Ghosh <Ghoshsoumyaneel@gmail.com>
2025-02-24 20:34:13 +0530

    done

---
+++ bio.java
@@ -0,0 +1,11 @@
+class bio
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Name : Soumyaneel Ghosh");
+		System.out.println("Aggregate : 9.05 CGPA");
+		System.out.println("Marks : 83");
+		System.out.println("Gender : Male");
+		System.out.println("Phone Number : 8017397606");
+	}
+}

5b023055cec1d3a0953abb820305366ded3eb79b
Soumyaneel Ghosh <Ghoshsoumyaneel@gmail.com>
2025-03-21 12:14:45 +0530

    update

---
+++ PrimeSum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class PrimeSum{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m= sc.nextInt();
+		int sum=0;
+		for (int x=2; x<=m; x++ ) {
+		int count=0;
+			for(int i = 1; i<=x; i++)
+			{
+				if(x%i==0)
+				count++;
+			}
+		
+		 if (count==2)
+		 sum = sum+x;
+		
+		}
+		 System.out.println(sum);
+		
+	}
+}
---
+++ TypeCast.java
@@ -0,0 +1,13 @@
+class TypeCast{
+public static void main(String[] args) {
+	float a=2.3f;
+	float b=4.2f;
+	float c=11.4f;
+	float d=3.2f;
+	int sum= (int) (a+b+c);
+	int res= (int)(sum+d);
+
+	System.out.println(res);
+	
+}
+}

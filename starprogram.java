2fe73a9a72d503235aac5ceb314262661ec342a1
Soumyaneel Ghosh <Ghoshsoumyaneel@gmail.com>
2025-04-04 12:26:23 +0530

    update

--- starprogram.java
+++ starprogram.java
@@ -1,9 +1,18 @@
+ //*
+// **
+// ***
+
 class Starprogram
 {
 	public static void main(String[] args)
 	{
-		System.out.println("*");
-		System.out.println("**");
-		System.out.print("***");
+		for (int i=0; i<3;i++ ) {
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		
+			
+		}
 	}
 }
